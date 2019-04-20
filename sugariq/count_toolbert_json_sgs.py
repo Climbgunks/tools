212ed9f789531b3ae5144a74b406d7d5469cfe7a
Todd Mummert <mummert@us.ibm.com>
2019-04-20 10:04:08 -0400

    count toolbert sgs

---
+++ sugariq/count_toolbert_json_sgs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='count sgs per day')
+parser.add_argument('-f','--fname', required=True, help='toolbert json input file')
+args = parser.parse_args()
+
+with open(args.fname) as f:
+    js = json.load(f)
+
+sg_counts = {}
+for entry in js:
+    for v in entry['value']:
+        for sg in v['sgs']:
+            date = sg['dateTime'].split('T')[0]
+            if date not in sg_counts:
+                sg_counts[date] = 1
+            else:
+                sg_counts[date] += 1
+
+print(json.dumps(sg_counts, indent=2, sort_keys=True))
+
+
+            
+
+            
