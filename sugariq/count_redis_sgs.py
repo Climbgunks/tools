62934213767af08f89346fc2af50af7353010b58
Todd Mummert <mummert@us.ibm.com>
2019-04-20 15:52:40 -0400

    utility for counting redis sgs

---
+++ sugariq/count_redis_sgs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--fname')
+args = parser.parse_args()
+
+
+with open(args.fname) as f:
+    js = json.load(f)
+
+day_cnt = {}
+for day in js:
+    for e in day:
+        #print(json.dumps(e, indent=2))
+        date = e['PUMP_TIME'].split('T')[0]
+        if 'intValues' in e and 'SG_mgdL' in e['intValues']:
+            if date not in day_cnt:
+                day_cnt[date] = 1
+            else:
+                day_cnt[date] += 1
+
+
+
+print(json.dumps(day_cnt, indent=2, sort_keys=True))
+
+            
+    
